4949c5504079b20591cf3deedfbdc9b44d7b273e
trentholla <trenthollandsworth@gmail.com>
2017-04-25 02:18:58 -0400

    put more detail in the graphs and added a general final grade bar plot

--- initial_eda.R
+++ initial_eda.R
@@ -18,17 +18,25 @@ group_by(data, G3) %>%
 data <- data[data$G3 != 0,]
 data <- data[data$G3 != 1,]
 
+# final grades
+ggplot(data, aes(x=G3)) + geom_bar() + 
+    labs(x = 'Final Grade', y = '# of Students', title = 'Distribution of Final Grades')
 
 # weekly study time
-ggplot(data, aes(x = studytime, y = G3))+ geom_jitter() + geom_smooth(method = "lm")
+ggplot(data, aes(x = studytime, y = G3, color = class, shape = class))+ geom_jitter() + 
+    geom_smooth(method = "lm") + 
+    labs(x = 'Weekly Study Time', y = 'Final Grade', title = 'Studying Improves Final Grades')
 
 # workday alcohol consumption
-ggplot(data, aes(x = Dalc, y = G3)) +  geom_jitter() + geom_smooth(method = "lm")
+ggplot(data, aes(x = Dalc, y = G3, color = class, shape = class)) +  geom_jitter() + geom_smooth(method = "lm") +
+    labs(x = 'Workday Alcohol Consumption', y = 'Final Grade', 
+         title = 'Weekday Drinking Harms Final Grades')
 
-# weekday alcohol consumption
-ggplot(data, aes(x = Walc, y = G3)) +  geom_jitter() + geom_smooth(method = "lm")
+# weekend alcohol consumption
+ggplot(data, aes(x = Walc, y = G3, color = class, shape = class)) +  geom_jitter() + geom_smooth(method = "lm") +
+    labs(x = 'Weekend Alcohol Consumption', y = 'Final Grade', 
+         title = 'Weekend Drinking Harms Final Grades')
 
 # health
-ggplot(data, aes(x = health, y = G3)) +  geom_jitter() + geom_smooth(method = "lm")
-
-
+ggplot(data, aes(x = health, y = G3, color = class, shape = class)) +  geom_jitter() + geom_smooth(method = "lm") +
+    labs(x = 'Health Status', y = 'Final Grade', title = 'Final Grade Not Impacted by Health')
