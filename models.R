8fb49eed19f6716a3df3bed9904f1d4529dd46ca
trentholla <trenthollandsworth@gmail.com>
2017-04-25 14:31:24 -0400

    touching up the format and adding to the rmd file

--- models.R
+++ models.R
@@ -1,7 +1,7 @@
-## Add possible models or work on variable selection here!
 library(tidyverse)
 library(car)
 
+#training and test sets ------------------------------------------------
 # there are n observations
 n <- dim(hour)[1]
 
@@ -17,55 +17,48 @@ train <- hour[tr_indices, ]
 test <- hour[-tr_indices, ]
 
 
-#comprehensive linear model 
-woah <- lm(G3 ~ school + sex + age + address + famsize + Pstatus + Medu + Fedu + Mjob + Fjob + 
+#comprehensive linear model ----------------------------- 
+model1 <- lm(G3 ~ school + sex + age + address + famsize + Pstatus + Medu + Fedu + Mjob + Fjob + 
                reason + guardian + traveltime + studytime + failures + schoolsup + famsup + 
                paid + activities + nursery + higher + internet + romantic + famrel + freetime + 
                goout + Dalc + Walc + health + absences + G1 + G2 + class, train)
-summary(woah)
-vif(woah)
-MSE1 <- mean(woah$residuals^2)
+summary(model1)
+vif(model1)
+MSE1 <- mean(model1$residuals^2)
 
 #previous test scores have big impact, good health results in lower test scores?
 #also, hypothesis confirmed that students who pay for classes do worse, no doubt due to selection bias
 
 
-#other models
-woah2 <- lm(G3 ~ age + schoolsup + paid + absences + G1 + G2 + class, train)
-summary(woah2)
-MSE2 <- mean(woah2$residuals^2)
+#other models -------------------------------------------
+model2 <- lm(G3 ~ age + schoolsup + paid + absences + G1 + G2 + class, train)
+summary(model2)
+MSE2 <- mean(model2$residuals^2)
 
-woah3 <- lm(G3 ~ G1 + G2 + class, train, na.action = na.omit)
-summary(woah3)
-MSE3 <- mean(woah3$residuals^2)
+model3 <- lm(G3 ~ G1 + G2 + class, train, na.action = na.omit)
+summary(model3)
+MSE3 <- mean(model3$residuals^2)
 
-woah4 <- lm(G3 ~ age + schoolsup + paid + absences + G1 + G2 +I(G1^2) + I(G2^2) + class, train)
-summary(woah4)
-MSE4 <- mean(woah4$residuals^2)
+model4 <- lm(G3 ~ age + schoolsup + paid + absences + G1 + G2 +I(G1^2) + I(G2^2) + class, train)
+summary(model4)
+MSE4 <- mean(model4$residuals^2)
 
-woah5 <- lm(G3 ~ absences + G1 + G2 + I(G1*G2) + class, train)
-summary(woah5)
-MSE5 <- mean(woah5$residuals^2)
+model5 <- lm(G3 ~ absences + G1 + G2 + I(G1*G2) + class, train)
+summary(model5)
+MSE5 <- mean(model5$residuals^2)
 
-models <- list(woah, woah2, woah3, woah4, woah5)
-#error
-error <- tibble(degree=c(1,2,3,4,5),
+models <- list(model1, model2, model3, model4, model5)
+#error -----------------------------------------
+error <- tibble(model_numberr=c(1,2,3,4,5),
                 MSE_tr=c(MSE1, MSE2, MSE3, MSE4, MSE5))
 
 ggplot(error)+
-    geom_point(aes(x=degree, y=MSE_tr)) +
-    geom_line(aes(x=degree, y=MSE_tr)) +
+    geom_point(aes(x=model_number, y=MSE_tr)) +
+    geom_line(aes(x=model_number, y=MSE_tr)) +
     labs(x = 'Model Number', y = 'Training Error', title = 'Training Error for the Models')
-
-#testing
-new_grade <- predict(woah3, newdata = test)
-test <- test %>% 
-    mutate(grade_pred=exp(new_grade))
-print(test)
 
 error <- error %>% 
     add_column(MSE_tst=rep(0, 5))
-
 
 for(i in 1:5){
     
